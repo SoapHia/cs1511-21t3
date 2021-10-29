cd7b824b97265e79c72fbf0b460b2d859dee5a4b
Sunny Chen <z5267282@student.unsw.edu.au>
2021-10-29 15:51:56 +1100

    Exercises 2-9 from tut07

---
+++ wk07/string_demo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+
+    // using array notation
+    char string1[20] = {'h', 'e', 'l', 'l', 'o', '\0'};
+
+    // using string shorthand
+    char string2[20] = "hello";
+
+    // a string literal
+    char *string_literal = "hello champion";
+
+    // INVALID STRING!
+    char bad[5] = "hello";
+
+}
+
